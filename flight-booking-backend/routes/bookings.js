52184e7f3feb1c7e050ee3e120cac48de14f6034
dilipksahu <sahud048@gmail.com>
2022-07-31 16:55:24 +0530

    swagger add and city name modified

--- flight-booking-backend/routes/bookings.js
+++ flight-booking-backend/routes/bookings.js
@@ -2,17 +2,112 @@ const router = require("express-promise-router")();
 const BookingsController = require("../controller/bookings");
 
 router
+
   .route("/")
+  /**
+   * @swagger
+   * /bookings/(bookingId):
+   *  get:
+   *    summary: Get all booking details
+   *    description: Used to get all the booking details for a certain booking id
+   *    responses:
+   *      '200':
+   *        description: Got all booking details successfully
+   *      '500':
+   *        description: Server error
+   *  parameters:
+   *       - in: path
+   *         name: bookingId
+   *         required: true
+   *         schema:
+   *         type: String
+   *         description: The booking ID
+   */
   .get(BookingsController.getAllBookings)
+  /**
+   * @swagger
+   * /bookings/:
+   *   post:
+   *     tags:
+   *       - Booking
+   *     description: Save booking details for a user
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: newBookingDetail
+   *         description: Booking object
+   *         in: body
+   *         required: true
+   *     responses:
+   *       200:
+   *         description: Return saved booking
+   */
   .post(BookingsController.addNewBooking);
 
 router
   .route("/:bookingId")
+  /** 
+  * @swagger
+  * /bookings/{id}:
+  *  get:
+  *    summary: Fetch a booking.
+  *    description: Used to fetch a single booking
+  *    responses:
+  *      '200':
+  *        description: Successfully fetched booking
+  *      '500':
+  *        description: Server error
+  *  parameters:
+  *       - in: path
+  *         name: id
+  *         required: true
+  *         schema:
+  *         type: String
+  *         description: The booking ID
+  */
   .get(BookingsController.getBookingById)
+  /** 
+  * @swagger
+  * /bookings/{id}:
+ *  delete:
+ *    summary: Delete a booking.
+ *    description: Used to delete a booking
+ *    responses:
+ *      '200':
+ *        description: Successfully deleted booking
+ *      '500':
+ *        description: Server error
+ *  parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *         type: String
+ *         description: The booking ID
+ */
   .delete(BookingsController.cancelBooking);
 
 router
   .route("/userDetails/:userDetailId")
+  /**
+  * @swagger 
+  * /bookings/{userDetailId}:
+ *  get:
+ *    summary: Fetch a booking.
+ *    description: Used to fetch a all bookings by userDetailId
+ *    responses:
+ *      '200':
+ *        description: Successfully fetched bookings
+ *      '500':
+ *        description: Server error
+ *  parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *         type: String
+ *         description: The booking ID
+ */
   .get(BookingsController.getUserDetailBookings);
 
 router.route("/razorpay").post(BookingsController.payment);
